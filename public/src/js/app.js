aa0a0d919ced597834aeafe91056e03fcb403749
[stepsen89] <stephanie.senoner@gmail.com>
2018-04-29 12:05:34 +0200

    rewriting promises, fetch in serviceworker.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -17,3 +17,32 @@ window.addEventListener('beforeinstallprompt', function(event){
   deferredPrompt = event;
   return false;
 })
+
+var promise = new Promise(function(resolve, reject){
+  setTimeout(function(){
+    //resolve('This is after timer is done');
+    reject({code: 500, message: 'An error occurred'});
+    //console.log('timer is done');
+  }, 3000);
+});
+
+// promise.then(function(text){
+//   return text;
+// }, function (err) {
+//   console.log(err.code, err.message)
+// }
+// }).then(function(newText) {
+//   console.log(newText);
+// })
+
+promise.then(function(text){
+  return text;
+}).then(function(newText) {
+  console.log(newText);
+}).catch(function(err){
+  console.log(err.code, err.message);
+})
+
+// value of promises if you have more asynchronous code 
+
+console.log('after settimeout');
