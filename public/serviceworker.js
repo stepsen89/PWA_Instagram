a6b8de7393f4dd9ef2d9ee660c0594ba4c9586dc
[stepsen89] <stephanie.senoner@gmail.com>
2018-04-29 11:11:52 +0200

    service worker registering and activating

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,1 +1,12 @@
-// register the service worker in every page
+// register the service worker in every 
+
+self.addEventListener('install', function(event){
+  console.log('Service Worker* Installing Service Worker ...', event);
+})
+
+self.addEventListener('activate', function(event){
+  console.log('Service Worker* Activating Service Worker ...', event);
+  return self.clients.claim();
+});
+// that SW loaded correctly (and activated)
+// in dev mode update on reload - not in production mode
